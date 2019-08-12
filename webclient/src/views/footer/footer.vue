d21524d98d7938f825cdfbbf85731b7682f0314c
evaseemefly by 7530 <evaseemefly@126.com>
2019-08-12 17:01:46 +0800

    1- init前端client 2- 引入了vscode自定义代码段 3- 解决了vue-cli3创建项目不支持vue-property-decorator的问题

---
+++ webclient/src/views/footer/footer.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+</div>
+</template>
+<script lang="ts"
+>
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+@Component({})
+export default class footer extends Vue {
+mydata: any = null;
+mounted() {}
+get computedTest(){
+return null;
+}
+}
+</script>
+<style scoped>
+</style>

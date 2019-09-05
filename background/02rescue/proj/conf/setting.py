56baa7fab57c6aa80bf8d32ea51c09371b045927
evaseemefly <evaseemefly@126.com>
2019-09-05 15:40:30 +0800

    1- 重新修改写入操作

--- background/02rescue/proj/conf/setting.py
+++ background/02rescue/proj/conf/setting.py
@@ -6,5 +6,9 @@ _MONGODB_PORT = 27017
 
 # -----
 # 路径相关
-DIR = r"/Users/liusihan/Documents/01project/SearchRescueSys/data/搜救结果"
+# mac 15
+# DIR = r"/Users/liusihan/Documents/01project/SearchRescueSys/data/搜救结果"
+# mac 13
+DIR = r"/Users/drno/Documents/01Proj/SearchRescueSys/data/demo_data"
+
 FILE_NAME = 'qz_ty1_100p.nc'
--- background/02rescue/proj/core/data.py
+++ background/02rescue/proj/core/data.py
@@ -34,6 +34,7 @@ class SearchRescueData(BaseData):
     '''
 
     ds = None
+    dict_dimension = {}
 
     def __init__(self, dir: str, file: str):
         super(SearchRescueData, self).__init__(dir, file)
@@ -44,8 +45,14 @@ class SearchRescueData(BaseData):
             根据fullpath，读取对应的文件
         :return:
         '''
+        # 初始化ds以及维度字典
         if (self.ds is None):
             self.ds = nc.Dataset(self.fullpath)
+            print(self.ds)
+            print('-----')
+            print(self.ds.variables)
+        if (self.dict_dimension == {}):
+            self.init_dimensions_dict()
 
     def checkNotMatchVariables(self, *args):
         '''
@@ -71,23 +78,55 @@ class SearchRescueData(BaseData):
             # 写入数据库
             # 1 先连接数据库
             my_connet()
-            for i in range(len(all_data['lat'])):
-                wind_temp = model.WindModel(x=all_data['xwind'][i], y=all_data['ywind'][i])
-                current_temp = model.CurrentModel(x=all_data['xcurrent'][i], y=all_data['ycurrent'][i])
-                # point = [all_data['lon'][i], all_data['lat'][i]]
-                point = [round(all_data['lon'][i].item(), 2), round(all_data['lat'][i].item(), 2)]
-                # point = [float(all_data['lon'][i]), float(all_data['lat'][i])]
-                # 注意此处的是np.float32类型，需要转换为float使用 .item()即可
-                # TODO:[-] 注意使用mongoengine中的预定义的point类型，只接受python原生的float类型，不支持float32类型
-                search_model = model.SearchRescueModel(time=all_data['time'][i], point=point, current=current_temp,
-                                                       wind=wind_temp, status=all_data['status'][i])
-                # mongoengine.errors.ValidationError: ValidationError(SearchRescueModel:None) (Both
-                #                                                                              values ([118.9177, 24.735289]) in point
-                # must
-                # be
-                # float or int: ['point'])
-                search_model.save()
+            # for i in range(len(all_data['lat'])):
+            #     wind_temp = model.WindModel(x=all_data['xwind'][i], y=all_data['ywind'][i])
+            #     current_temp = model.CurrentModel(x=all_data['xcurrent'][i], y=all_data['ycurrent'][i])
+            #     # point = [all_data['lon'][i], all_data['lat'][i]]
+            #     point = [round(all_data['lon'][i].item(), 2), round(all_data['lat'][i].item(), 2)]
+            #     # point = [float(all_data['lon'][i]), float(all_data['lat'][i])]
+            #     # 注意此处的是np.float32类型，需要转换为float使用 .item()即可
+            #     # TODO:[-] 注意使用mongoengine中的预定义的point类型，只接受python原生的float类型，不支持float32类型
+            #     search_model = model.SearchRescueModel(time=all_data['time'][i], point=point, current=current_temp,
+            #                                            wind=wind_temp, status=all_data['status'][i])
+            #     # mongoengine.errors.ValidationError: ValidationError(SearchRescueModel:None) (Both
+            #     #                                                                              values ([118.9177, 24.735289]) in point
+            #     # must
+            #     # be
+            #     # float or int: ['point'])
+            #     search_model.save()
+            # TODO:[*] 19-09-06 此处重新修改了，因为要根据两维进行遍历所以上面的方式不再适用
+            self.circulation()
             # pass
+
+    def circulation(self):
+        '''
+            循环写入
+        :return:
+        '''
+        x_index = 0
+        y_index = 0
+
+        for y_trajectory_temp in range(self.get_y_trajectory - 1):
+            # 0-99
+
+            for x_time_temp in range(self.get_x_time - 1):
+                # 0-24
+                wind_temp = model.WindModel(x=self.ds['x_wind'][y_trajectory_temp, x_time_temp],
+                                            y=self.ds['y_wind'][y_trajectory_temp, x_time_temp])
+                current_temp = model.CurrentModel(x=self.ds['x_sea_water_velocity'][y_trajectory_temp, x_time_temp],
+                                                  y=self.ds['x_sea_water_velocity'][y_trajectory_temp, x_time_temp])
+                # point = [all_data['lon'][i], all_data['lat'][i]]
+                point_temp = [round(self.ds['lon'][y_trajectory_temp, x_time_temp].item(), 2),
+                              round(self.ds['lat'][y_trajectory_temp, x_time_temp].item(), 2)]
+                time_temp = self.get_time_data[x_time_temp]
+                status_temp = self.ds['status'][y_trajectory_temp, x_time_temp]
+                search_model = model.SearchRescueModel(time=time_temp, point=point_temp, current=current_temp,
+                                                       wind=wind_temp, status=status_temp)
+                search_model.save()
+
+                x_index = x_index + 1
+            y_index = y_index + 1
+            pass
 
     def get_all_data(self, type):
         all_data_dict = {'lat': self.get_lat_data,
@@ -136,3 +175,35 @@ class SearchRescueData(BaseData):
     @property
     def get_ycurrent_data(self):
         return self.ds['y_sea_water_velocity'][:][0].data
+
+    def init_dimensions_dict(self):
+        '''
+            初始化维度字典
+        :return:
+        '''
+        for index, temp in enumerate(self.ds.dimensions.values()):
+            print(temp)
+            self.dict_dimension[temp.name] = temp
+        # if self.dict_dimension == {}:
+        #     for temp in self.ds.dimension.values():
+        #         print(temp)
+        # self.dict_dimension[temp.name] = temp
+        # return self.dict_dimension
+
+    @property
+    def get_y_trajectory(self):
+        '''
+            获取 轨迹 维度 的长度
+        :return:
+        '''
+        # 正常有100组
+        return self.dict_dimension['trajectory'].size
+
+    @property
+    def get_x_time(self):
+        '''
+            获取 time 维度 的长度
+        :return:
+        '''
+        # 正常有25个
+        return self.dict_dimension['time'].size

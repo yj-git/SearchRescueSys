5a290870f44c35f4e5228a82bbd448d0cb2bb371
evaseemefly-aw <evaseemefly@126.com>
2019-11-14 22:46:51 +0800

    1- 作业页面加入编辑按钮 2- content top加入统计信息

--- webclient/src/views/content/CaseListContent/CaseList.vue
+++ webclient/src/views/content/CaseListContent/CaseList.vue
@@ -1,59 +1,82 @@
 <template>
   <div id="case_list_content">
-    <el-table :data="tableData" border style="width: 100%">
-      <el-table-column label="日期" width="180">
-        <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span style="margin-left: 10px">{{ scope.row.date }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="作业名" width="180">
-        <template slot-scope="scope">
-          <el-popover trigger="hover" placement="top">
-            <p>作业名: {{ scope.row.name }}</p>
-            <!-- <p>住址: {{ scope.row.address }}</p> -->
-            <div slot="reference" class="name-wrapper">
-              <el-tag size="medium">{{ scope.row.name }}</el-tag>
-            </div>
-          </el-popover>
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" width="180">
-        <template slot-scope="scope">
-          <el-popover trigger="hover" placement="top">
-            <p>状态: {{ scope.row.state }}</p>
-            <!-- <p>住址: {{ scope.row.address }}</p> -->
-            <div slot="reference" class="name-wrapper">
-              <!-- 通过设置type，设置'primary'为蓝色，'success'为绿色，也可以设置为其他颜色，待试验 -->
-              <el-tag
-                size="medium"
-                :type="scope.row.tag === 'finish' ? 'primary' : 'success'"
-              >{{ scope.row.state }}</el-tag>
-            </div>
-          </el-popover>
-        </template>
-      </el-table-column>
-      <el-table-column label="区域" width="180">
-        <template slot-scope="scope">
-          <el-popover trigger="hover" placement="top">
-            <p>区域: {{ scope.row.area }}</p>
-            <!-- <p>住址: {{ scope.row.address }}</p> -->
-            <div slot="reference" class="name-wrapper">
-              <el-tag size="medium">{{ scope.row.area }}</el-tag>
-            </div>
-          </el-popover>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button size="mini" type="success" @click="handleEdit(scope.$index, scope.row)">加载</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="job-statistics"></div>
+    <div class="user-content">
+      <!-- 按钮区域 -->
+      <div class="buttons">
+        <el-row>
+          <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        </el-row>
+      </div>
+      <div class="my-table">
+        <el-table :data="tableData" border style="width: 100%">
+          <el-table-column label="日期" width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="作业名" width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>作业名: {{ scope.row.name }}</p>
+                <!-- <p>住址: {{ scope.row.address }}</p> -->
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="状态" width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>状态: {{ scope.row.state }}</p>
+                <!-- <p>住址: {{ scope.row.address }}</p> -->
+                <div slot="reference" class="name-wrapper">
+                  <!-- 通过设置type，设置'primary'为蓝色，'success'为绿色，也可以设置为其他颜色，待试验 -->
+                  <el-tag
+                    size="medium"
+                    :type="scope.row.tag === 'finish' ? 'primary' : 'success'"
+                    >{{ scope.row.state }}</el-tag
+                  >
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="区域" width="180">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>区域: {{ scope.row.area }}</p>
+                <!-- <p>住址: {{ scope.row.address }}</p> -->
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.area }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="success"
+                @click="handleEdit(scope.$index, scope.row)"
+                >加载</el-button
+              >
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)"
+                >删除</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 加载当前的作业 -->
+    </div>
   </div>
 </template>
-<script lang='ts'>
+<script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 @Component({})
 export default class center_map extends Vue {
@@ -65,7 +88,7 @@ export default class center_map extends Vue {
       address: "上海市普陀区金沙江路 1518 弄",
       state: "作业中",
       tag: "doing",
-      area:'ind'
+      area: "ind"
     },
     {
       date: "2016-05-04",
@@ -73,7 +96,7 @@ export default class center_map extends Vue {
       address: "上海市普陀区金沙江路 1518 弄",
       state: "排队中",
       tag: "wait",
-      area:'scs'
+      area: "scs"
     },
     {
       date: "2016-05-01",
@@ -81,7 +104,7 @@ export default class center_map extends Vue {
       address: "上海市普陀区金沙江路 1518 弄",
       state: "已结束",
       tag: "finish",
-      area:'bhs'
+      area: "bhs"
     },
     {
       date: "2016-05-03",
@@ -89,7 +112,7 @@ export default class center_map extends Vue {
       address: "上海市普陀区金沙江路 1518 弄",
       state: "排队中",
       tag: "wait",
-      area:'ecs'
+      area: "ecs"
     }
   ];
   mounted() {}
@@ -98,8 +121,40 @@ export default class center_map extends Vue {
   }
 }
 </script>
-<style scoped>
+<style scoped lang="less">
+@import "../../../styles/base.less";
+
 #case_list_content {
   width: 100%;
+  display: flex;
+  padding: 0.5em;
+  // 用户功能区
+  .user-content {
+    display: flex;
+
+    flex: 1;
+    flex-direction: column;
+    // border-bottom-right-radius: 0.5em;
+    // border-bottom-left-radius: 0.5em;
+    padding-bottom: 1em;
+    background: orange;
+    @bottomradius();
+    // 按钮区
+    .buttons {
+      display: flex;
+      flex: 2;
+      background: rgba(119, 119, 219, 0.801);
+
+      .el-row {
+        @button();
+      }
+    }
+    // 用户的table
+    .my-table {
+      display: flex;
+      flex: 10;
+      background: rgba(107, 197, 197, 0.801);
+    }
+  }
 }
 </style>

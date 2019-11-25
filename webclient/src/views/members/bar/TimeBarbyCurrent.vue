f9a6784d97c77b24e33be408f32b2a86ed5bd655
evaseemefly by 7530 <evaseemefly@126.com>
2019-11-25 09:54:09 +0800

    1- 家中timebarbycurrent

---
+++ webclient/src/views/members/bar/TimeBarbyCurrent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="time_bar">
+      <div class="timebar container">
+        <input class="form-control col-md-3" aria-describedby="emailHelp" v-model="current" />
+        <button type="button" class="btn btn-primary" @click="setCurrentDate()">搜索</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { Mutation, State, namespace } from "vuex-class";
+@Component({})
+export default class TimeBarbyCurrent extends Vue {
+  mydata: any = null;
+  current: String = "1990-01-01T6:00:00.0Z";
+  // 修改当前的current的值
+  setCurrentDate() {
+    // console.log(val);
+    // console.log(this.current);
+    var tempCurrent = this.current;
+    // this.$store.commit("current", tempCurrent);
+    this.setCurrent(tempCurrent);
+  }
+
+  @Mutation("setcurrent", { namespace: "map" }) setCurrent;
+  mounted() {}
+  get computedTest() {
+    return null;
+  }
+}
+</script>
+<style scoped>
+#time_bar {
+  display: flex;
+  flex: 10;
+  background: rgba(210, 219, 77, 0.829);
+  height: 50px;
+}
+.timebar {
+  /* 横向排列即可 */
+  display: flex;
+}
+</style>

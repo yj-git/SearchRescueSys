7d1b6223bb4726f51bfe70456be41eacbae6e12e
evaseemefly-t5820 <evaseemefly@126.com>
2020-01-06 16:51:26 +0800

    1- 合并了cwb提交的直接读取nc文件的方式

--- background/04byRabbitmq/job/NetCDF_IO.py
+++ background/04byRabbitmq/job/NetCDF_IO.py
@@ -9,6 +9,7 @@ import xarray as xar
 from django.db import models
 from mongoengine import *
 
+
 class MassModel(EmbeddedDocument):
     '''
        质量相关model
@@ -19,6 +20,7 @@ class MassModel(EmbeddedDocument):
     evaporated = FloatField(default=None)
     # 分散的质量
     dispersed = FloatField(default=None)
+
 
 class CurrentModel(EmbeddedDocument):
     '''
@@ -35,6 +37,7 @@ class WindModel(EmbeddedDocument):
     x = FloatField()
     y = FloatField()
 
+
 class OilModel(EmbeddedDocument):
     '''
         油相关的model
@@ -43,6 +46,7 @@ class OilModel(EmbeddedDocument):
     density = FloatField()
     # 油膜的厚度
     film_thickness = FloatField()
+
 
 class OilSpillingModel(Document):
     '''
@@ -62,6 +66,7 @@ class OilSpillingModel(Document):
 
     oil = EmbeddedDocumentField(OilModel)
 
+
 def getMassOilLst(ncFileName, nTime, lstVariables, nan):
     try:
         ds = nc.Dataset(ncFileName)
@@ -70,38 +75,44 @@ def getMassOilLst(ncFileName, nTime, lstVariables, nan):
         for i in range(len(lstVariables)):
             # mass_oil = ds_xr.isel(time=1)['mass_oil']
             # print(mass_oil.where(mass_oil != 9.969210e+36).to_dataframe().dropna(how='any'))
-            if(i>0):
+            if (i > 0):
                 xr_var = ds_xr.isel(time=nTime)[lstVariables[i]]
-                #print(xr_var)
+                # print(xr_var)
                 xr_variables = xar.merge([xr_variables, xr_var])
-        df_variables = xr_variables.where(xr_variables!= 9.969210e+36).to_dataframe().dropna(how='any')
+        df_variables = xr_variables.where(xr_variables != 9.969210e+36).to_dataframe().dropna(how='any')
         return df_variables
-    except Exception:
+    except Exception as ex:
+        print(ex)
         return None
     # finally:
     # return None
 
-if __name__ == '__main__':
+
+def main():
     while (1):
-        ncFileName = 'e:/data/sanjioil.nc'
+        ncFileName = r'D:\02proj\new_SearchRescueSys\SearchRescueSys\background\01byJupyter\data\sanjioil.nc'
         nTime = 30
-        lstVariables = ['mass_oil' , 'x_sea_water_velocity', 'y_sea_water_velocity', 'x_wind', 'y_wind']
-        #lstVariables = ['x_wind', 'y_wind']
+        lstVariables = ['mass_oil', 'x_sea_water_velocity', 'y_sea_water_velocity', 'x_wind', 'y_wind']
+        # lstVariables = ['x_wind', 'y_wind']
         nan = 9.969210e+36
         df_variables = getMassOilLst(ncFileName, nTime, lstVariables, nan)
         print(df_variables)
-        #ds = nc.Dataset(file_path)
-        #ds_xr = xar.open_dataset(file_path)
-        #print(ds_xr.dims)
-        #print(ds_xr)
-        #print(ds_xr.coords)
+        # ds = nc.Dataset(file_path)
+        # ds_xr = xar.open_dataset(file_path)
+        # print(ds_xr.dims)
+        # print(ds_xr)
+        # print(ds_xr.coords)
 
-        #获取几个指定维度的的DataArray
+        # 获取几个指定维度的的DataArray
         # 读取一个 wind
-        #mass_oil = ds_xr.isel(time=1)['mass_oil']
-        #print(mass_oil.where(mass_oil != 9.969210e+36).to_dataframe().dropna(how='any'))
+        # mass_oil = ds_xr.isel(time=1)['mass_oil']
+        # print(mass_oil.where(mass_oil != 9.969210e+36).to_dataframe().dropna(how='any'))
 
-        #x_wind = ds_xr.isel(time=1)['x_wind']
-        #print(x_wind.to_dataframe())
-        #print(x_wind.where(x_wind != 9.969210e+36).to_dataframe().dropna(how='any'))
+        # x_wind = ds_xr.isel(time=1)['x_wind']
+        # print(x_wind.to_dataframe())
+        # print(x_wind.where(x_wind != 9.969210e+36).to_dataframe().dropna(how='any'))
         time.sleep(300)
+
+
+if __name__ == '__main__':
+    main()

7842516fd83853f5d42b9f095d2207279059de0f
evaseemefly <evaseemefly@126.com>
2020-02-06 09:50:24 +0800

    1- p52s

--- webserver/Search_Rescue/Search_Rescue/settings.py
+++ webserver/Search_Rescue/Search_Rescue/settings.py
@@ -99,10 +99,10 @@ DATABASES = {
         # by casablanca
         'USER': 'root',  # 账号
         # 7530
-        'PASSWORD': 'admin123',
+        # 'PASSWORD': 'admin123',
         # 5820
         # 5510
-        # 'PASSWORD': '123456',
+        'PASSWORD': '123456',
         # by cwb
         # 'USER': 'root',  # 账号
         # 'PASSWORD': '123456',

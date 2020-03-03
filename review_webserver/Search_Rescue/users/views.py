e4c8f7420b99defb2f4f2871e6feffa0cf536da9
tongtong1121 <19345288@qq.com>
2020-03-03 13:54:06 +0800

    2月25日 TODO修改： 1、在user/model.py中增加提交参数验证 2、在user/model.py中自动生成case_code、root_path、case_path等参数 3、删除user/views.py中提交job、case的update功能

--- review_webserver/Search_Rescue/users/views.py
+++ review_webserver/Search_Rescue/users/views.py
@@ -119,8 +119,12 @@ class CaseListView(APIView):
         if len(relas) > 0:
             # 找到所有的有关的case info
             for rela_temp in relas:
-                if hasattr(rela_temp, 'did'):
-                    case_temp: ICaseBaseModel = getattr(rela_temp, 'did')
+                # caiwb 20-02-29
+                # if hasattr(rela_temp, 'did'):
+                #     case_temp: ICaseBaseModel = getattr(rela_temp, 'did')
+                #     cases.append(case_temp)
+                if hasattr(rela_temp, 'did_id'):
+                    case_temp: ICaseBaseModel = getattr(rela_temp, 'did_id')
                     cases.append(case_temp)
         return cases
 
@@ -170,6 +174,16 @@ class CaseListView(APIView):
         json_data = JobMidSerializer(jobs_mid, many=True)
         return Response(json_data.data)
 
+    def gen_casecode(self, code):
+        '''
+            根据传入的 code 参数，生成带时间戳的case_code编码
+        :param code:
+        :return:
+        '''
+        dt_ms = datetime.now().strftime('%Y%m%d%H%M%S%f')
+        case_code = code + '_' + dt_ms
+        return case_code
+
     def set_caseinfo(self, request, uid: str):
         '''
             根据传入的 uid 与 request 添加或更新对应的case
@@ -183,115 +197,53 @@ class CaseListView(APIView):
         # TODO:[*] 20-02-25 以下部分建议改为通过model进行获取对应的参数，在model中添加验证方法，不用放在view
         # TODO:[*] 20-02-25 建议将root_path 与 case_path 通过工厂模式实现创建对应的 路径(路径不由前端传入，路径在后端根据传入的 user name 与当前的 yyyy/mm/dd 共同拼接而成。
         # eg: /user_name/yyyy/mm/dd/
-        root_path = request.GET.get('root_path', None)
-        case_path = request.GET.get('case_path', None)
-        temp_date = request.GET.get('create_date', None)
-        if temp_date is not None:
-            create_date = datetime.strptime(temp_date, '%Y-%m-%d %H:%M:%S')
-        else:
-            create_date = None
-        temp_date = request.GET.get('forecast_date', None)
-        if temp_date is not None:
-            forecast_date = datetime.strptime(temp_date, '%Y-%m-%d %H:%M:%S')
-        else:
-            forecast_date = None
-        case_name = request.GET.get('case_name', None)
-        case_desc = request.GET.get('case_desc', None)
-        case_code = request.GET.get('case_code', None)
-        lat = request.GET.get('lat', None)
-        if lat is not None:
-            lat = float(lat)
-        lon = request.GET.get('lon', None)
-        if lon is not None:
-            lon = float(lon)
-        radius = request.GET.get('radius', None)
-        if radius is not None:
-            radius = float(radius)
-        nums = request.GET.get('nums', None)
-        if nums is not None:
-            nums = int(nums)
-        simulation_duration = request.GET.get('simulation_duration', None)
-        if simulation_duration is not None:
-            simulation_duration = float(simulation_duration)
-        simulation_step = request.GET.get('simulation_step', None)
-        if simulation_step is not None:
-            simulation_step = float(simulation_step)
-        console_step = request.GET.get('console_step', None)
-        if console_step is not None:
-            console_step = float(console_step)
-        current_nondeterminacy = request.GET.get('current_nondeterminacy', None)
-        if current_nondeterminacy is not None:
-            current_nondeterminacy = float(current_nondeterminacy)
-        equation = request.GET.get('equation', None)
-        if equation is not None:
-            equation = int(equation)
-        is_del = request.GET.get('is_del', None)
-        if is_del == '0':
-            is_del = False
-        elif is_del == '1':
-            is_del = True
-        else:
-            is_del = None
-        area = request.GET.get('area', None)
-        wind_coefficient = request.GET.get('wind_coefficient', None)
-        if wind_coefficient is not None:
-            wind_coefficient = float(wind_coefficient)
-        wind_dir = request.GET.get('wind_dir', None)
-        if wind_dir is not None:
-            wind_dir = float(wind_dir)
+        attrs = {}
+        users = User.objects.filter(id=uid)
+        attrs['root_path'] = users[0].username
+        attrs['case_path'] = datetime.now().strftime('%Y/%m')
+        attrs['create_date'] = request.GET.get('create_date', None)
+        attrs['forecast_date'] = request.GET.get('forecast_date', None)
+        attrs['case_name'] = request.GET.get('case_name', None)
+        attrs['case_desc'] = request.GET.get('case_desc', None)
+        attrs['case_code'] = request.GET.get('case_code', None)
+        attrs['lat'] = request.GET.get('lat', None)
+        attrs['lon'] = request.GET.get('lon', None)
+        attrs['radius'] = request.GET.get('radius', None)
+        attrs['nums'] = request.GET.get('nums', None)
+        attrs['simulation_duration'] = request.GET.get('simulation_duration', None)
+        attrs['simulation_step'] = request.GET.get('simulation_step', None)
+        attrs['console_step'] = request.GET.get('console_step', None)
+        attrs['current_nondeterminacy'] = request.GET.get('current_nondeterminacy', None)
+        attrs['equation'] = request.GET.get('equation', None)
+        attrs['is_del'] = request.GET.get('is_del', None)
+        attrs['area'] = request.GET.get('area', None)
+        attrs['wind_coefficient'] = request.GET.get('wind_coefficient', None)
+        attrs['wind_dir'] = request.GET.get('wind_dir', None)
         try:
             # TODO:[*] 20-02-25此处验证操作放在 对应的 model中进行验证
-            if None not in [type, root_path, case_path, create_date, forecast_date, case_name, case_desc, case_code,
-                    lat, lon, radius, nums, simulation_duration, simulation_step, console_step,
-                    current_nondeterminacy, equation, is_del, area, wind_coefficient, wind_dir]:
-                cases = self.get_caseinfo(uid, type)
-                update_num = 0
-                #如果存在相同的case_code的记录，则更新CaseOilInfo相关字段值
-                if len(cases) > 0:
-                    for case_temp in cases:
-                        if case_temp.case_code == case_code:
-                            update_num = CaseOilInfo.objects.filter(id=case_temp.id).update(root_path=root_path, case_path=case_path,
-                                                                     create_date=create_date,
-                                                                     forecast_date=forecast_date,
-                                                                     case_name=case_name, case_desc=case_desc,
-                                                                     case_code=case_code,
-                                                                     lat=lat, lon=lon, radius=radius, nums=nums,
-                                                                     simulation_duration=simulation_duration,
-                                                                     simulation_step=simulation_step,
-                                                                     console_step=console_step,
-                                                                     current_nondeterminacy=current_nondeterminacy,
-                                                                     equation=equation, is_del=is_del,
-                                                                     area=area,
-                                                                     wind_coefficient=wind_coefficient,
-                                                                     wind_dir=wind_dir)
-
-                            break
-                # 如果不存在相同的case_code的记录，则创建CaseOilInfo记录
-                if update_num == 0:
-                    case_result = CaseOilInfo.objects.create(root_path=root_path, case_path=case_path,
-                                                             create_date=create_date,
-                                                             forecast_date=forecast_date,
-                                                             case_name=case_name, case_desc=case_desc,
-                                                             case_code=case_code,
-                                                             lat=lat, lon=lon, radius=radius, nums=nums,
-                                                             simulation_duration=simulation_duration,
-                                                             simulation_step=simulation_step,
-                                                             console_step=console_step,
-                                                             current_nondeterminacy=current_nondeterminacy,
-                                                             equation=equation, is_del=is_del,
-                                                             area=area,
-                                                             wind_coefficient=wind_coefficient,
-                                                             wind_dir=wind_dir)
-
+            attrs = CaseOilInfo.validate(self, attrs)
+            if attrs is not None:
+                # 创建CaseOilInfo记录
+                case_result = CaseOilInfo.objects.create(root_path=attrs['root_path'],
+                                        case_path=attrs['case_path'], create_date=attrs['create_date'], forecast_date=attrs['forecast_date'],
+                                        case_name=attrs['case_name'], case_desc=attrs['case_desc'], case_code=attrs['case_code'],
+                                        lat=attrs['lat'], lon=attrs['lon'], radius=attrs['radius'], nums=attrs['nums'], simulation_duration=attrs['simulation_duration'],
+                                        simulation_step=attrs['simulation_step'], console_step=attrs['console_step'], current_nondeterminacy=attrs['current_nondeterminacy'],
+                                        equation=attrs['equation'], is_del=attrs['is_del'], area=attrs['area'], wind_coefficient=attrs['wind_coefficient'],
+                                        wind_dir=attrs['wind_dir'])
+                if case_result is not None:
+                    # 建立User、Case关系
                     if type == 0:
                         model = AuthOilRela
                     else:
                         model = AuthRescueRela
-                    rela = model.objects.filter(uid_id=uid, did_id=case_result.id)
-                    if len(rela) == 0:
-                        model.objects.create(uid_id=uid, did_id=case_result.id)
-            if case_result is not None:
-                return case_result
+                    rela = model.objects.create(uid_id=uid, did_id=case_result.id)
+                    if rela is not None:
+                        return case_result
+                    else:
+                        return None
+                else:
+                    return None
         except:
             pass
 
@@ -303,58 +255,34 @@ class CaseListView(APIView):
         :return:
         '''
         type = int(request.GET.get('type', None))
-        job_celery_id = request.GET.get('job_celery_id', None)
-        # TODO:[*] 20-02-25 此处需要对case_code进行加密(现在的case_code为 'xx')，需要在追加一个唯一的字符串 'xx'->'xx_afhjkashfjkas'，最好创建一个方法，根据时间戳或者其他方式生成唯一的字符串标识码
-        case_code = request.GET.get('case_code', None)
-        temp_date = request.GET.get('gmt_create', None)
-        if temp_date is not None:
-            gmt_create = datetime.strptime(temp_date, '%Y-%m-%d %H:%M:%S')
-        else:
-            gmt_create = None
-        temp_date = request.GET.get('gmt_modified', None)
-        if temp_date is not None:
-            gmt_modified = datetime.strptime(temp_date, '%Y-%m-%d %H:%M:%S')
-        else:
-            gmt_modified = None
-        is_del = request.GET.get('is_del', None)
-        if is_del is not None:
-            if is_del == '0':
-                is_del = False
-            elif is_del == '1':
-                is_del = True
-            else:
-                is_del = None
-        area = request.GET.get('area', None)
-        if area is not None:
-            area = int(area)
 
+        # TODO:[*] 20-02-25 此处需要对case_code进行加密(现在的case_code为 'xx')，需要在追加一个唯一的字符串 'xx'->'xx_afhjkashfjkas'，最好创建一个方法，根据时间戳或者其他方式生成唯一的字符串标识码
+        attrs = {}
+        attrs['job_celery_id'] = request.GET.get('job_celery_id', None)
+        attrs['case_code'] = request.GET.get('case_code', None)
+        attrs['gmt_create'] = request.GET.get('gmt_create', None)
+        attrs['gmt_modified'] = request.GET.get('gmt_modified', None)
+        attrs['is_del'] = request.GET.get('is_del', None)
+        attrs['area'] = request.GET.get('area', None)
         # TODO:[*] 20-02-25 rate不由前端提交，首次创建完 user_jobinfo 后之后，在 user_jobuserrate 中添加对应的初始记录(此时rate应为0)，以后更新rate在后台中执行
-        rate = request.GET.get('rate', None)
-        if rate is not None:
-            rate = int(rate)
-        state = request.GET.get('state', None)
-        if state is not None:
-            state = int(state)
+        attrs['rate'] = 0
+        attrs['state'] = request.GET.get('state', None)
         try:
-            if None not in [type, job_celery_id, case_code, gmt_create, gmt_modified, is_del, area]:
-                # 如果存在相同的case_code的记录，则更新JobInfo相关字段值
-                jobinfo_result = JobInfo.objects.filter(case_code=case_code)
-                if len(jobinfo_result) > 0:
-                    update_num = jobinfo_result.update(type=type, job_celery_id=job_celery_id, gmt_create=gmt_create,
-                                                        gmt_modified=gmt_modified, is_del=is_del, area=area)
-                    if update_num > 0:
+            attrs = JobInfo.validate(self, attrs)
+            if attrs is not None:
+                # 创建JobInfo记录
+                # TODO:[-] 20-02-25 添加JobInfo的同时还需要在 user_caseoilinfo 中添加相应记录——已在 set_caseinfo 方法中实现
+                jobinfo_result = JobInfo.objects.create(type=type, job_celery_id=attrs['job_celery_id'], case_code=attrs['case_code'],
+                                 gmt_create=attrs['gmt_create'], gmt_modified=attrs['gmt_modified'], is_del=attrs['is_del'], area=attrs['area'])
+                if jobinfo_result is not None:
+                    jobrate_result = JobUserRate.objects.create(uid_id=uid, jid_id=jobinfo_result.id, rate=attrs['rate'], state=attrs['state'],
+                                                    gmt_create=attrs['gmt_create'], gmt_modified=attrs['gmt_modified'])
+                    if jobrate_result is not None:
                         return jobinfo_result
+                    else:
+                        return None
                 else:
-                    # 如果不存在相同的case_code的记录，则创建JobInfo记录
-                    # TODO:[-] 20-02-25 添加JobInfo的同时还需要在 user_caseoilinfo 中添加相应记录——已在 set_caseinfo 方法中实现
-                    jobinfo_result = JobInfo.objects.create(type=type, job_celery_id=job_celery_id, case_code=case_code,
-                                                        gmt_create=gmt_create, gmt_modified=gmt_modified, is_del=is_del, area=area)
-                    if jobinfo_result is not None:
-                        if None not in [state, rate]:
-                            jobrate_result = JobUserRate.objects.create(uid_id=uid, jid_id=jobinfo_result.id, rate=rate, state=state,
-                                                        gmt_create=gmt_create, gmt_modified=gmt_modified)
-                            if jobrate_result is not None:
-                                return jobinfo_result
+                    return None
         except:
             pass
 

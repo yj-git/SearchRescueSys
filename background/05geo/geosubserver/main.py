592de0abacebef2e5f91d5a39defbe5fcb1fca08
evaseemefly-p500 <evaseemefly@126.com>
2020-03-10 21:44:11 +0800

    1- 完成了 创建ncstore 2- 完成 创建coverage(需要再完善)

--- background/05geo/geosubserver/main.py
+++ background/05geo/geosubserver/main.py
@@ -6,6 +6,7 @@ def main():
     cat.connect()
     cat.get_workspace('ceshi')
     cat.create_ncstore('nmefc_2016072112_opdr', 'my_test_2', 'nmefc/waterwind')
+    cat.create_coverage('my_test_2', 'ceshi_coverage_01', 'nmefc_2016072112_opdr')
     pass
 
 

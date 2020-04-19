a00ffdef30184bbdd931c8d8024114dd3be66d02
evaseemefly-mac <evaseemefly@126.com>
2020-04-19 15:17:47 +0800

    去掉敏感信息

--- background/06byDelayTask/proj/conf/settings.py
+++ background/06byDelayTask/proj/conf/settings.py
@@ -8,15 +8,15 @@
 # @Software: PyCharm
 
 _FTP = {
-    '_URL': '128.5.6.142',
-    '_USERNAME': 'Currents',
-    '_PWD': '123'
+    '_URL': '',
+    '_USERNAME': '',
+    '_PWD': ''
 }
 # 增加风场FTP地址，caiwb 2020-04-13
 _FTP_WIND = {
-    '_URL': '128.5.6.142',
-    '_USERNAME': 'wind',
-    '_PWD': '123'
+    '_URL': '',
+    '_USERNAME': '',
+    '_PWD': ''
 }
 # 用来存储 ftp 下载的文件的根目录(或放在数据库中？)
 # DOWNLOAD_ROOT = r'D:\01proj\SearchRescueSys\data\download'
